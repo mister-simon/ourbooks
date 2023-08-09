d6a90ac40577483a6939f2562aeb1978c245bd03
Simon W <simon@apphaus.co.uk>
2023-08-09 12:57:10 +0100

    Adds some new toys to play with and gets them working again.

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
+</html>
