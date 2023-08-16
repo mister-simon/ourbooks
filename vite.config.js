9ab2208ea8436e776c3d0389f110aa04a0b4d061
Simon W <simon@apphaus.co.uk>
2023-08-16 15:10:01 +0100

    Trying to get refresh delay working... But it doesn't.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
-            refresh: ['storage/framework/views/**', 'resources/sass/app.scss', 'resources/js/app.js']
+            refresh: [{
+                paths: refreshPaths,
+                config: { delay: 1000 }
+            }],
         }),
     ],
 })
