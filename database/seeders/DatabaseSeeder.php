9461949de1fffbc55af546267d15868e5fe5f06e
Simon W <simon@apphaus.co.uk>
2023-08-16 17:24:09 +0100

    Adds a way to import book data from a csv

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\Models\Book;
 use App\Models\Shelf;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use League\Csv\Reader;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +18,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // CSV import
+        $importCsv = database_path('seeders/import.csv');
 
+        if (File::exists($importCsv)) {
+            $user = User::factory()
+                ->has(Shelf::factory(['title' => 'Our Shelf']))
+                ->create([
+                    'email' => 'simon@example.com',
+                    'name' => 'Simon',
+                ]);
+
+            $shelf = $user->shelves->first();
+
+            $reader = Reader::createFromPath($importCsv, 'r')
+                ->setHeaderOffset(0);
+
+            $attributeMap = [
+                'Author Surname' => 'author_surname',
+                'Author Forename' => 'author_forename',
+                'Order' => 'series_index',
+                'Book Title' => 'title',
+                'Genre' => 'genre',
+                'Edition' => 'edition',
+                'Co Author' => 'co_author',
+            ];
+
+            foreach ($reader->getRecords() as $record) {
+                $data = collect($record)
+                    ->mapWithKeys(
+                        fn ($value, $header) => [
+                            $attributeMap[trim($header)] ?? trim($header) => trim($value),
+                        ]
+                    )
+                    ->transform(
+                        fn ($value, $attribute) => $attribute === 'series_index'
+                            ? ($value === '' ? null : (int) $value)
+                            : $value
+                    )
+                    ->all();
+
+                $shelf->books()->create($data);
+            }
+        }
+
+        // Basic data seed
         User::factory()
             ->has(
                 Shelf::factory()
@@ -25,8 +71,7 @@ class DatabaseSeeder extends Seeder
                             ->count(150)
                     )
             )
-            ->create([
-                'email' => 'arst@arst.arst',
-            ]);
+            ->create(['email' => 'test@example.com']);
+
     }
 }
