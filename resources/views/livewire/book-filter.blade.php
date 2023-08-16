60847c6c2a2c269baaa64a58c4b3948ef772e04b
Simon W <simon@apphaus.co.uk>
2023-08-16 15:15:48 +0100

    Simplify book filter

--- resources/views/livewire/book-filter.blade.php
+++ resources/views/livewire/book-filter.blade.php
@@ -3,18 +3,8 @@
 use App\Models\Book;
 use function Livewire\Volt\{state, rules, updated};
 
-state(['series', 'series_index', 'author_surname', 'author_forename', 'title', 'genre', 'edition', 'co_author']);
-
-rules([
-    'series' => ['nullable', 'string'],
-    'series_index' => ['nullable', 'integer', 'min:0'],
-    'author_surname' => ['nullable', 'string'],
-    'author_forename' => ['nullable', 'string'],
-    'title' => ['nullable', 'string'],
-    'genre' => ['nullable', 'string'],
-    'edition' => ['nullable', 'string'],
-    'co_author' => ['nullable', 'string'],
-]);
+state(['search']);
+rules(['search' => ['nullable', 'string']]);
 
 $filter = function () {
     $data = $this->validate();
@@ -22,16 +12,7 @@ $filter = function () {
     $this->dispatch('book-filter', filter: $this->all());
 };
 
-updated([
-    'series' => fn() => $this->filter(),
-    'series_index' => fn() => $this->filter(),
-    'author_surname' => fn() => $this->filter(),
-    'author_forename' => fn() => $this->filter(),
-    'title' => fn() => $this->filter(),
-    'genre' => fn() => $this->filter(),
-    'edition' => fn() => $this->filter(),
-    'co_author' => fn() => $this->filter(),
-]);
+updated(['search' => fn() => $this->filter()]);
 
 ?>
 
@@ -42,14 +23,7 @@ updated([
 
     <form wire:submit="filter">
         <div class="grid grid-cols-4 gap-2">
-            <x-text-input name="title" wire:model.live="title" />
-            <x-text-input name="author_surname" wire:model.live="author_surname" />
-            <x-text-input name="author_forename" wire:model.live="author_forename" />
-            <x-text-input name="co_author" wire:model.live="co_author" />
-            <x-genre-input name="genre" wire:model.live="genre" />
-            <x-text-input name="series" wire:model.live="series" />
-            <x-number-input name="series_index" wire:model.live="series_index" />
-            <x-text-input name="edition" wire:model.live="edition" />
+            <x-text-input name="search" wire:model.live.debounce="search" />
         </div>
 
         <x-button type="submit">Search</x-button>
