967b50eb8e4ee97d7d523374100ad49012d55487
Simon W <simon@apphaus.co.uk>
2023-08-16 17:27:42 +0100

    Move chunk of import logic out

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,53 +13,13 @@ use League\Csv\Reader;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // CSV import
         $importCsv = database_path('seeders/import.csv');
 
         if (File::exists($importCsv)) {
-            $user = User::factory()
-                ->has(Shelf::factory(['title' => 'Our Shelf']))
-                ->create([
-                    'email' => 'simon@example.com',
-                    'name' => 'Simon',
-                ]);
-
-            $shelf = $user->shelves->first();
-
-            $reader = Reader::createFromPath($importCsv, 'r')
-                ->setHeaderOffset(0);
-
-            $attributeMap = [
-                'Author Surname' => 'author_surname',
-                'Author Forename' => 'author_forename',
-                'Order' => 'series_index',
-                'Book Title' => 'title',
-                'Genre' => 'genre',
-                'Edition' => 'edition',
-                'Co Author' => 'co_author',
-            ];
-
-            foreach ($reader->getRecords() as $record) {
-                $data = collect($record)
-                    ->mapWithKeys(
-                        fn ($value, $header) => [
-                            $attributeMap[trim($header)] ?? trim($header) => trim($value),
-                        ]
-                    )
-                    ->transform(
-                        fn ($value, $attribute) => $attribute === 'series_index'
-                            ? ($value === '' ? null : (int) $value)
-                            : $value
-                    )
-                    ->all();
-
-                $shelf->books()->create($data);
-            }
+            $this->importCsv($importCsv);
         }
 
         // Basic data seed
@@ -72,6 +32,47 @@ class DatabaseSeeder extends Seeder
                     )
             )
             ->create(['email' => 'test@example.com']);
+    }
 
+    protected function importCsv($path)
+    {
+        $user = User::factory()
+            ->has(Shelf::factory(['title' => 'Our Shelf']))
+            ->create([
+                'email' => 'simon@example.com',
+                'name' => 'Simon',
+            ]);
+
+        $shelf = $user->shelves->first();
+
+        $reader = Reader::createFromPath($path, 'r')
+            ->setHeaderOffset(0);
+
+        $attributeMap = [
+            'Author Surname' => 'author_surname',
+            'Author Forename' => 'author_forename',
+            'Order' => 'series_index',
+            'Book Title' => 'title',
+            'Genre' => 'genre',
+            'Edition' => 'edition',
+            'Co Author' => 'co_author',
+        ];
+
+        foreach ($reader->getRecords() as $record) {
+            $data = collect($record)
+                ->mapWithKeys(
+                    fn ($value, $header) => [
+                        $attributeMap[trim($header)] ?? trim($header) => trim($value),
+                    ]
+                )
+                ->transform(
+                    fn ($value, $attribute) => $attribute === 'series_index'
+                        ? ($value === '' ? null : (int) $value)
+                        : $value
+                )
+                ->all();
+
+            $shelf->books()->create($data);
+        }
     }
 }
