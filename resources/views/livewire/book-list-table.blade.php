f16c36ca5fe269ef03068e7f3fbd93821b52d763
Simon W <simon@apphaus.co.uk>
2023-08-21 16:03:50 +0100

    Adds character separation to table list view

--- resources/views/livewire/book-list-table.blade.php
+++ resources/views/livewire/book-list-table.blade.php
@@ -11,9 +11,9 @@ state(['books' => fn() => $books]);
         <thead class="border-b font-medium dark:border-neutral-500">
             <tr>
                 <td scope="col" class="px-6 py-2">Surname</td>
-                <td scope="col" class="px-6 py-2">Forename</td>
+                <td scope="col" class="border-r px-6 py-2">Forename</td>
                 <td scope="col" class="px-6 py-2">Series</td>
-                <td scope="col" class="px-6 py-2">Title</td>
+                <td scope="col" class="border-r px-6 py-2">Title</td>
                 <td scope="col" class="px-6 py-2">Genre</td>
                 <td scope="col" class="px-6 py-2">Edition</td>
                 <td scope="col" class="px-6 py-2">Co Author</td>
@@ -21,6 +21,11 @@ state(['books' => fn() => $books]);
         </thead>
         <tbody>
             @forelse ($this->books as $book)
+                @if (($prev = $this->books[$loop->index - 1] ?? null) === null || $book->author_surname_char !== $prev->author_surname_char)
+                    <tr>
+                        <td colspan="100%" class="whitespace-nowrap border-y bg-neutral-100 px-6 text-center transition duration-300 ease-in-out dark:bg-neutral-600">{{ $book->author_surname_char }}</td>
+                    </tr>
+                @endif
                 @if ($next = $this->books[$loop->index + 1] ?? null)
                 @endif
                 <tr class="@if ($next && $next->authorName !== $book->authorName) border-neutral-500 dark:border-neutral-200 @else border-neutral-200 dark:border-neutral-500 @endif whitespace-nowrap border-b px-6 py-2 transition duration-300 ease-in-out hover:bg-neutral-100 dark:hover:bg-neutral-600">
