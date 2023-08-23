67ddacf53966bce7f8d8584e1af525eeca5bbaf3
Simon W <simon@apphaus.co.uk>
2023-08-23 17:24:59 +0100

    Adds basics for book policy

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -3,33 +3,17 @@
 namespace App\Policies;
 
 use App\Models\Book;
+use App\Models\Shelf;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class BookPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Book $book): bool
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user, Shelf $shelf): bool
     {
-        //
+        return $user->hasVerifiedEmail() && $shelf->users->contains($user);
     }
 
     /**
@@ -37,7 +21,9 @@ class BookPolicy
      */
     public function update(User $user, Book $book): bool
     {
-        //
+        $shelf = $book->shelf;
+
+        return $user->hasVerifiedEmail() && $shelf->users->contains($user);
     }
 
     /**
@@ -45,22 +31,8 @@ class BookPolicy
      */
     public function delete(User $user, Book $book): bool
     {
-        //
-    }
+        $shelf = $book->shelf;
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Book $book): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Book $book): bool
-    {
-        //
+        return $user->hasVerifiedEmail() && $shelf->users->contains($user);
     }
 }
