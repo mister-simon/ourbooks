bd050820988da1b5f6825031c5ca5ba478bd33ea
Simon W <simon@apphaus.co.uk>
2023-08-23 17:27:09 +0100

    Better support for light mode and learning about listening for events from alpine.

--- resources/views/livewire/book-list-shelf-book.blade.php
+++ resources/views/livewire/book-list-shelf-book.blade.php
@@ -3,7 +3,7 @@
 use function Livewire\Volt\{state, computed};
 
 state(['book' => fn() => $book])->locked();
-state(['colors' => ['border-slate-600 bg-slate-600', 'border-slate-900 bg-slate-900', 'border-zinc-500 bg-zinc-500', 'border-neutral-600 bg-neutral-600', 'border-neutral-800 bg-neutral-800', 'border-orange-800 bg-orange-800', 'border-green-800 bg-green-800', 'border-sky-800 bg-sky-800', 'border-rose-800 bg-rose-800', 'border-teal-800 bg-teal-800', 'border-teal-900 bg-teal-900', 'border-green-900 bg-green-900']]);
+state(['colors' => ['border-slate-300 bg-slate-300 dark:border-slate-600 dark:bg-slate-600', 'border-slate-300 bg-slate-100 dark:border-slate-900 dark:bg-slate-900', 'border-zinc-300 bg-zinc-200 dark:border-zinc-500 dark:bg-zinc-500', 'border-neutral-300 bg-neutral-300 dark:border-neutral-600 dark:bg-neutral-600', 'border-neutral-300 bg-neutral-200 dark:border-neutral-800 dark:bg-neutral-800', 'border-orange-300 bg-orange-200 dark:border-orange-800 dark:bg-orange-800', 'border-green-300 bg-green-200 dark:border-green-800 dark:bg-green-800', 'border-sky-300 bg-sky-200 dark:border-sky-800 dark:bg-sky-800', 'border-rose-300 bg-rose-200 dark:border-rose-800 dark:bg-rose-800', 'border-teal-300 bg-teal-200 dark:border-teal-800 dark:bg-teal-800', 'border-teal-300 bg-teal-200 dark:border-teal-900 dark:bg-teal-900', 'border-green-300 bg-green-200 dark:border-green-900 dark:bg-green-900']])->locked();
 
 $colorIndex = computed(function () {
     $length = count($this->colors);
@@ -16,10 +16,15 @@ $color = computed(function () {
 
 ?>
 
-<div class="{{ $this->color }} relative mb-2 flex shrink flex-row flex-wrap border border-b-0" x-data="{ open: false }">
+<div
+    class="{{ $this->color }} relative mb-2 flex shrink flex-row flex-wrap border border-b-0"
+    x-data="{ open: false }"
+    :class="open && 'grow'">
     <button
-        class="flex max-h-40 grow items-center justify-center self-stretch px-1 py-4 dark:bg-white/10"
-        @click="open = !open">
+        class="flex max-h-40 items-center justify-center self-stretch bg-black/5 px-1 py-4 dark:bg-white/10"
+        @click="open = !open"
+        x-on:books-list-expand.window="open = true"
+        x-on:books-list-collapse.window="open = false">
         <span class="text-mode-vertical line-clamp-3 overflow-hidden text-ellipsis">
             {{ $book->title }}
         </span>
@@ -27,7 +32,7 @@ $color = computed(function () {
     <div
         x-cloak
         x-show="open"
-        class="custom-scrollbar max-h-40 origin-left overflow-auto p-4"
+        class="custom-scrollbar max-h-40 grow origin-left overflow-auto p-4"
         x-transition:enter="transition-all ease-out duration-200"
         x-transition:enter-start="opacity-0 transform scale-x-50"
         x-transition:enter-end="opacity-100 transform scale-x-100"
@@ -51,5 +56,5 @@ $color = computed(function () {
             @endforeach
         </div>
     </div>
-    <hr class="absolute -inset-x-0 top-full w-screen -translate-x-1/2 border-b-4 border-t-4 border-b-amber-950 border-t-amber-900" role="presentation">
+    <hr class="absolute -inset-x-0 top-full w-[200vw] -translate-x-1/2 border-b-4 border-t-4 border-b-amber-950 border-t-amber-900" role="presentation">
 </div>
--- resources/views/livewire/book-list-shelf.blade.php
+++ resources/views/livewire/book-list-shelf.blade.php
@@ -8,23 +8,43 @@ $groupedBooks = computed(fn() => $this->books->groupBy('authorSurnameChar'));
 
 ?>
 
-<div class="flex flex-wrap items-end justify-center gap-y-10 overflow-hidden border-8 border-amber-950 bg-amber-800/10 py-10 shadow-inner" id="shelf">
-    @foreach ($this->groupedBooks as $char => $group)
-        <div class="relative -mt-10 mb-auto flex flex-row flex-wrap self-stretch px-1">
-            <span class="clip-b-arrow absolute top-0 -translate-x-1/2 border border-neutral-900 border-b-transparent bg-neutral-950 px-1 pb-[5px] font-mono text-sm font-bold">
-                {{ $char }}
-            </span>
-        </div>
-        @foreach ($group as $book)
-            <livewire:book-list-shelf-book :book="$book" />
-        @endforeach
-    @endforeach
+<div class="relative">
+    {{-- Controls --}}
+    <div class="sticky inset-x-0 top-16 z-20 mb-4 ml-4 drop-shadow">
+        <x-button
+            class="inline-flex gap-2 !px-2 shadow-lg"
+            wire:click="$dispatch('books-list-expand')">
+            @svg('heroicon-o-chevron-double-right', 'w-4 h-4') Expand All
+        </x-button>
+        <x-button
+            class="inline-flex gap-2 !px-2 shadow-lg"
+            wire:click="$dispatch('books-list-collapse')">
+            @svg('heroicon-o-chevron-double-left', 'w-4 h-4') Collapse All
+        </x-button>
+    </div>
 
-    @if ($this->books->isEmpty())
-        <div class="relative -mt-10 mb-auto flex grow flex-row flex-wrap self-stretch px-1">
-            <span class="clip-b-arrow absolute top-0 border border-neutral-900 border-b-transparent bg-neutral-950 px-1 pb-4 font-mono text-sm font-bold">
-                No books yet...
-            </span>
-        </div>
-    @endif
+    <div class="flex flex-wrap items-end justify-center gap-y-10 overflow-hidden border-8 border-amber-950 bg-amber-800/10 py-10 shadow-inner" id="shelf">
+
+        {{-- Books by Surname char --}}
+        @foreach ($this->groupedBooks as $char => $group)
+            <div class="relative -mt-10 mb-auto flex flex-row flex-wrap self-stretch px-1 drop-shadow-[0_1px_1px_#000]">
+                <span class="clip-b-arrow absolute top-0 -translate-x-1/2 border border-b-transparent bg-neutral-100 px-1 pb-[5px] font-mono text-sm font-bold dark:border-neutral-900 dark:bg-neutral-950">
+                    {{ $char }}
+                </span>
+            </div>
+            @foreach ($group as $book)
+                <livewire:book-list-shelf-book :book="$book" />
+            @endforeach
+        @endforeach
+
+        {{-- No books --}}
+        @if ($this->books->isEmpty())
+            <div class="relative -mt-10 mb-auto flex flex-row flex-wrap self-stretch drop-shadow-[0_1px_1px_#000]">
+                <span class="clip-b-arrow absolute top-0 mx-1 border border-b-transparent bg-neutral-100 px-1 pb-4 font-mono text-sm font-bold dark:border-neutral-900 dark:bg-neutral-950">
+                    No books here...
+                </span>
+            </div>
+            <div class="mt-40 grow border-b-4 border-t-4 border-b-amber-950 border-t-amber-900"></div>
+        @endif
+    </div>
 </div>
