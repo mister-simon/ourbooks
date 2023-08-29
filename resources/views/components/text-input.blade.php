8a9e507df565a8e81e7f567aedb9f0b06f706a4b
Simon W <simon@apphaus.co.uk>
2023-08-29 12:21:17 +0100

    Fixing border styling of inputs

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -6,6 +6,6 @@
         'placeholder' =>
             $label ??
             str($attributes->get('name'))->replace('_', ' ')->title(),
-        'class' => 'peer block min-h-[auto] w-full rounded border-neutral-200 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear',
+        'class' => 'peer block min-h-[auto] w-full rounded border border-neutral-200 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear',
     ]) }} />
 </x-form-group>
