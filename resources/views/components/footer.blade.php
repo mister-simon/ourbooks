c6da46cc900685a5c5a1ea686c9432f44fb21fbd
Simon W <simon@apphaus.co.uk>
2023-09-21 17:26:30 +0100

    Change footer position

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
-<div class="mt-auto flex">
+<div class="mt-auto flex items-end">
     <x-link class="m-2 inline-flex items-center gap-2 text-xs" href="{{ route('terms') }}">Terms Of Service</x-link>
     <x-link class="m-2 inline-flex items-center gap-2 text-xs" href="{{ route('privacy') }}">Privacy Policy</x-link>
 
-    <div class="alert alert-warning mx-auto mb-4 max-w-screen-sm text-xs">
+    <div class="alert alert-warning mx-auto mx-auto mb-4 max-w-screen-sm text-xs">
         <div class="space-y-2">
             <p>This site is still very much under construction, and is currently running as a personal project for me to mess around and learn some stuff.</p>
             <p>It generally works, but it's highly likely that things are going to change dramatically.</p>
