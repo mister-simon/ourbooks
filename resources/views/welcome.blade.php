e7a7a17c06b6a47bee8b099948ff1a59ca80107c
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-21 14:57:30 +0000

    Complete reset, starting to rework jetstream scaffolding.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<x-guest-layout>
+    @livewire('navigation-menu')
+
+    <div class="flex grow flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+        <div class="prose mt-6 overflow-hidden bg-white px-6 py-4 shadow-md sm:rounded-lg">
+            <div class="flex justify-center">
+                <x-authentication-card-logo alt="OurBooks.top logo" />
+            </div>
+            <p>What can OurBookstop do for you?</p>
+            <ul>
+                <li>Track your books</li>
+                <li>Easily search and filter your books</li>
+                <li>Share your list with friends and family</li>
+                <li>Collaboratively rate and review your books</li>
+            </ul>
+        </div>
+    </div>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,21 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    if (auth()->check()) {
+        return to_route('dashboard');
+    }
+
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
