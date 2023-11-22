7d55a65df020bea6ee3a9495bbfe8ee0954f55a6
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-22 17:06:12 +0000

    Adds basic books list

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,30 @@
 
     <div class="grow bg-gray-100 py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <ul class="menu">
+                @foreach (auth()->user()->shelves as $shelf)
+                    <li>
+                        {{ $shelf->title }}
+                        <ul class="menu">
+                            @foreach ($shelf->books()->orderBy('author_surname')->orderBy('author_forename')->orderBy('series')->orderBy('series_index')->orderBy('title')->with('bookUsers.user')->get() as $book)
+                                <li>
+                                    {{ $book->title }}
+                                    <ul class="menu">
+                                        <li>{{ $book->author_forename }} - {{ $book->author_surname }}</li>
+                                        @if ($book->co_author)
+                                            <li>{{ $book->co_author }}</li>
+                                        @endif
+                                        <li>{{ $book->series_text }}</li>
+                                        <li>{{ $book->title }}</li>
+                                        <li>{{ $book->genre }}</li>
+                                        <li>{{ $book->edition }}</li>
+                                    </ul>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 </x-app-layout>
