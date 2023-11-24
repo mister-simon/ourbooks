6047744c1ae6bc8bdf10c4a88b0a484dffe32270
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-24 17:47:49 +0000

    Adds border bottom to thead cells via after elements

--- resources/views/components/page-card.blade.php
+++ resources/views/components/page-card.blade.php
@@ -1,6 +1,6 @@
 <section class="grow bg-gray-100 py-12">
     <div class="mx-auto grid max-w-7xl sm:px-6 lg:px-8">
-        <div class="card bg-base-100 shadow-xl">
+        <div {{ $attributes->class('card bg-base-100 shadow-xl') }}>
             <div class="card-body">
                 {{ $slot }}
             </div>
--- resources/views/shelves/show.blade.php
+++ resources/views/shelves/show.blade.php
@@ -7,14 +7,14 @@
         </x-slot>
     </x-page-header>
 
-    <x-page-card>
+    <x-page-card class="card-compact">
         <table class="table table-zebra table-pin-rows table-sm">
             <thead>
-                <tr>
+                <tr class="[&>th]:after:absolute [&>th]:after:inset-x-0 [&>th]:after:bottom-0 [&>th]:after:block [&>th]:after:h-[1px] [&>th]:after:bg-neutral-700">
                     <th scope="col">Forename</th>
-                    <th scope="col" class="border-r">Surname</th>
+                    <th scope="col" class="border-r border-neutral-700">Surname</th>
                     <th scope="col">Series</th>
-                    <th scope="col" class="border-r">Title</th>
+                    <th scope="col" class="border-r border-neutral-700">Title</th>
                     <th scope="col">Genre</th>
                     <th scope="col">Edition</th>
                     <th scope="col">CoAuthor</th>
@@ -24,9 +24,9 @@
                 @foreach ($books as $book)
                     <tr>
                         <td>{{ $book->author_forename }}</td>
-                        <td class="border-r">{{ $book->author_surname }}</td>
+                        <td class="border-r border-neutral-700">{{ $book->author_surname }}</td>
                         <td>{{ $book->series_text }}</td>
-                        <td class="border-r">{{ $book->title }}</td>
+                        <td class="border-r border-neutral-700">{{ $book->title }}</td>
                         <td>{{ $book->genre }}</td>
                         <td>{{ $book->edition }}</td>
                         <td>{{ $book->co_author }}</td>
