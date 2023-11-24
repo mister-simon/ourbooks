42fef4d87c213707501e27b88bdef9d51bab4468
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-24 16:51:32 +0000

    Shelf show table updates / adds sticky header

--- resources/views/shelves/show.blade.php
+++ resources/views/shelves/show.blade.php
@@ -9,31 +9,31 @@
         <div class="mx-auto grid max-w-7xl sm:px-6 lg:px-8">
             <div class="card bg-base-100 shadow-xl">
                 <div class="card-body">
-                    <div class="overflow-x-auto">
-                        <table class="table table-zebra table-sm">
-                            <thead>
-                                <th scope="col" class="border-b">Forename</th>
-                                <th scope="col" class="border-b border-r">Surname</th>
-                                <th scope="col" class="border-b">Series</th>
-                                <th scope="col" class="border-b border-r">Title</th>
-                                <th scope="col" class="border-b">Genre</th>
-                                <th scope="col" class="border-b">Edition</th>
-                                <th scope="col" class="border-b">Co Author</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($books as $book)
-                                    <tr>
-                                        <td>{{ $book->author_forename }}</td>
-                                        <td class="border-r">{{ $book->author_surname }}</td>
-                                        <td>{{ $book->series_text }}</td>
-                                        <td class="border-r">{{ $book->title }}</td>
-                                        <td>{{ $book->genre }}</td>
-                                        <td>{{ $book->edition }}</td>
-                                        <td>{{ $book->co_author }}</td>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                    <table class="table table-zebra table-pin-rows table-sm">
+                        <thead>
+                            <tr>
+                                <th scope="col">Forename</th>
+                                <th scope="col" class="border-r">Surname</th>
+                                <th scope="col">Series</th>
+                                <th scope="col" class="border-r">Title</th>
+                                <th scope="col">Genre</th>
+                                <th scope="col">Edition</th>
+                                <th scope="col">CoAuthor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($books as $book)
+                                <tr>
+                                    <td>{{ $book->author_forename }}</td>
+                                    <td class="border-r">{{ $book->author_surname }}</td>
+                                    <td>{{ $book->series_text }}</td>
+                                    <td class="border-r">{{ $book->title }}</td>
+                                    <td>{{ $book->genre }}</td>
+                                    <td>{{ $book->edition }}</td>
+                                    <td>{{ $book->co_author }}</td>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
