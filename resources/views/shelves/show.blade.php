1d33f35027f214f30e8b4cbeec2f2e686545ddb1
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-27 15:17:04 +0000

    Adds character divider to table. Got way distracted working on this...

--- resources/views/shelves/show.blade.php
+++ resources/views/shelves/show.blade.php
@@ -26,11 +26,11 @@
     <x-page-card class="card-compact">
         <table class="table table-zebra table-pin-rows">
             <thead>
-                <tr class="[&>th]:after:absolute [&>th]:after:inset-x-0 [&>th]:after:-bottom-px [&>th]:after:block [&>th]:after:h-[1px] [&>th]:after:bg-neutral-700">
+                <tr class="z-10">
                     <th scope="col">Forename</th>
-                    <th scope="col" class="border-r border-neutral-700">Surname</th>
+                    <th scope="col">Surname</th>
                     <th scope="col">Series</th>
-                    <th scope="col" class="border-r border-neutral-700">Title</th>
+                    <th scope="col">Title</th>
                     <th scope="col">Genre</th>
                     <th scope="col">Edition</th>
                     <th scope="col">CoAuthor</th>
@@ -38,22 +38,38 @@
             </thead>
             <tbody>
                 @forelse ($books as $book)
-                    <tr>
-                        <td>{{ $book->author_forename }}</td>
-                        <td class="border-r border-neutral-700">{{ $book->author_surname }}</td>
-                        <td>{{ $book->series_text }}</td>
-                        <td class="border-r border-neutral-700">{{ $book->title }}</td>
-                        <td>{{ $book->genre }}</td>
-                        <td>{{ $book->edition }}</td>
-                        <td>{{ $book->co_author }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="100%" class="text-center">
-                            No books yet -
-                            <a href="{{ route('shelves.book.create', ['shelf' => $shelf]) }}" class="link">Add A Book</a>.
-                        </td>
-                    </tr>
+                    @if (($prevBook ?? null) && $book->author_surname_char !== $prevBook->author_surname_char)
+                        {{-- This is kinda jank but it interjects the surname character, which sticks underneath the header row --}}
+            </tbody>
+            <thead>
+                <tr class="top-6 z-20 bg-transparent">
+                    <th class=""></th>
+                    <th class="-translate-x-1 translate-y-2 py-0">
+                        <span class="badge badge-lg w-full rounded-b-lg rounded-t-none border-none px-1">{{ $book->author_surname_char }}</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @endif
+
+                {{-- Continue with book records --}}
+                <tr>
+                    <td>{{ $book->author_forename }}</td>
+                    <td>{{ $book->author_surname }}</td>
+                    <td>{{ $book->series_text }}</td>
+                    <td>{{ $book->title }}</td>
+                    <td>{{ $book->genre }}</td>
+                    <td>{{ $book->edition }}</td>
+                    <td>{{ $book->co_author }}</td>
+                </tr>
+                @php($prevBook = $book)
+            @empty
+                <tr>
+                    <td colspan="100%" class="text-center">
+                        No books yet -
+                        <a href="{{ route('shelves.book.create', ['shelf' => $shelf]) }}" class="link">Add A Book</a>.
+                    </td>
+                </tr>
                 @endforelse
             </tbody>
         </table>
