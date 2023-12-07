b27eaa6693c42b105a7fd5d67275ca7349576fbd
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-12-07 16:18:06 +0000

    Rm some things to slightly improve bundle size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ import typography from '@tailwindcss/typography';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
+        // './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        // './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
