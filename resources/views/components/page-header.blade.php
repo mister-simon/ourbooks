283f6b7c451fdc80a60ce4e9cc6c841ce274defb
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-12-07 16:18:21 +0000

    Header layout / shadow tweaks

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,5 +1,5 @@
 @php($breadcrumbs ??= new \Illuminate\View\ComponentSlot())
-<header class="bg-base-100 shadow">
+<header class="relative bg-base-100 drop-shadow-sm">
     <div class="container mx-auto flex flex-wrap items-center justify-between gap-4 px-4 py-6 sm:px-6 lg:px-8">
         <div class="flex flex-col gap-4">
             <h2 class="text-xl font-semibold leading-tight">{{ $slot }}</h2>
@@ -11,12 +11,10 @@
     </div>
 
     @if ($breadcrumbs->isNotEmpty())
-        <div class="bg-gray-100">
-            <div class="container mx-auto">
-                <nav {{ $breadcrumbs->attributes->class('breadcrumbs rounded-b-box bg-base-100 mx-4 sm:mx-6 lg:mx-8 px-4 pt-0 w-max text-sm')->merge(['aria-label' => 'Breadcrumb']) }}>
-                    {{ $breadcrumbs }}
-                </nav>
-            </div>
+        <div class="container absolute inset-x-0 top-full mx-auto">
+            <nav {{ $breadcrumbs->attributes->class('breadcrumbs rounded-b-box bg-base-100 mx-4 sm:mx-6 lg:mx-8 px-4 pt-0 w-max text-sm')->merge(['aria-label' => 'Breadcrumb']) }}>
+                {{ $breadcrumbs }}
+            </nav>
         </div>
     @endif
 </header>
