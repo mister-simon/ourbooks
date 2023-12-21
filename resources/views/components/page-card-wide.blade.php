936e1db5c6715aeb7b0c1215a85a3dc0fdf17e4e
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-12-21 15:36:10 +0000

    Widen wide page views

--- resources/views/components/page-card-wide.blade.php
+++ resources/views/components/page-card-wide.blade.php
@@ -1,7 +1,7 @@
 @php($section ??= new \Illuminate\View\ComponentSlot())
 @php($container ??= new \Illuminate\View\ComponentSlot())
 <section {{ $section->attributes->class('grow bg-gray-100 py-12') }}>
-    <div {{ $container->attributes->class('grid sm:px-6 lg:px-8') }}>
+    <div {{ $container->attributes->class('grid sm:px-4') }}>
         <div {{ $attributes->class('card bg-base-100 shadow-xl') }}>
             <div class="card-body">
                 {{ $slot }}
